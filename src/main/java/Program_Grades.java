13bf63878d6dcb6fe606c80dff32452a6fa93349
RandomLonelyDev <gedeon@porubannursery.com>
2022-10-05 12:08:02 -0400

    Added Program_Grades

---
+++ src/main/java/Program_Grades.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Program_Grades {
+    public static void main(String[] args) {
+        Scanner scanline = new Scanner(System.in);
+        while(true) {
+            try {
+                int eSum = 0, mSum = 0;
+                System.out.println("How many grades would you like to enter?");
+                int max = scanline.nextInt();
+                for (int i = 0; i < max; i++) {
+                    System.out.format("\nEnter grade %d\n", i + 1);
+                    eSum += scanline.nextInt();
+                    System.out.println("How many points was it worth?");
+                    mSum += scanline.nextInt();
+                }
+                double avg = (eSum*100.0)/mSum;
+                System.out.format("Your points - %d\nTotal points - %d\n\nYour average was %.2f\nYou earned a %s\n\n\n", eSum, mSum, avg, avg >= 90?"A":avg < 90 && avg >= 80?"B":avg < 80 && avg >= 70?"C":avg < 70 && avg >= 60?"D":"F");
+            } catch (InputMismatchException e) {
+                System.out.print("\nError!  Please enter a valid integer.  Press 'Enter' to continue.\n");
+                scanline.nextLine();
+                scanline.nextLine();
+            }
+        }
+    }
+}
--- src/main/java/Program_Guessing_Game_2.java
+++ src/main/java/Program_Guessing_Game_2.java
@@ -2,14 +2,24 @@ import java.util.*;
 public class Program_Guessing_Game_2 {
     public static void main(String[] args){
         int tries = 0,attempt = 0;
+        boolean won = false;
         Scanner scanline = new Scanner(System.in);
-        final int num = new Random().ints(1,101).findFirst().getAsInt();
-        while(attempt != num){
-            System.out.print("Enter a number between 1 and 100: ");
-            attempt = scanline.nextInt();
-            System.out.print(attempt > num?"Too high.\n":attempt < num?"Too low.\n":"You won!\n");
-            tries += 1;
+        while(!won) {
+            try {
+                final int num = new Random().ints(1, 6).findFirst().getAsInt();
+                while (attempt != num) {
+                    System.out.print("Enter a number between 1 and 100: ");
+                    attempt = scanline.nextInt();
+                    System.out.print(attempt > num ? "Too high.\n" : attempt < num ? "Too low.\n" : "You won!\n");
+                    tries += 1;
+                }
+                System.out.format("It took you %d guesses!", tries);
+                won = true;
+            } catch (InputMismatchException e) {
+                System.out.print("\nError! Please enter a valid integer between 1 and 100.  Press 'Enter' to continue.");
+                scanline.nextLine();
+                scanline.nextLine();
+            }
         }
-        System.out.format("It took you %d guesses!",tries);
     }
 }
