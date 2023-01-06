6d09d11608d4dfdd345b994a8fcc82b0841cdb7b
RandomLonelyDev <gedeon@porubannursery.com>
2023-01-05 23:09:15 -0500

    REST nightmare is over.  fuck this shit

--- src/main/java/personal_tests/wildfire/RESTtest.java
+++ src/main/java/personal_tests/wildfire/RESTtest.java
@@ -3,6 +3,7 @@ package personal_tests.wildfire;
 import org.igniterealtime.restclient.RestApiClient;
 import org.igniterealtime.restclient.entity.AuthenticationToken;
 import org.igniterealtime.restclient.entity.UserEntity;
+import org.igniterealtime.restclient.enums.SupportedMediaType;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -11,7 +12,8 @@ import java.net.URL;
 
 public class RESTtest {
     public static void main(String[] args) {
-        RestApiClient client = new RestApiClient("openxmpp",9090,new AuthenticationToken("bazre77fGnyVYTXo"));
-        client.getUser("admin").getUsername();
+        RestApiClient client = new RestApiClient("openxmpp",9090,new AuthenticationToken("bazre77fGnyVYTXo"), SupportedMediaType.JSON);
+        String us = client.getUser("admin").getUsername();
+        System.out.println(us);
     }
 }
